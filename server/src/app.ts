8e8fd0a495dc910eb0e287e90887edd9a98d76e0
zerowk <em@zerowk.it>
2023-06-08 10:13:47 +0200

    feat(project): initialize project

---
+++ server/src/app.ts
@@ -0,0 +1,24 @@
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import { connectToMongoDB } from "./config/connectionDB.config";
+
+const app = express();
+dotenv.config();
+
+app.use(cors());
+
+app.get("/", (_: Request, res: Response) => {
+  return res.status(200).send({
+    status: 200,
+    message: "Server is running",
+    endpoint: [],
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server started in port ${PORT} `);
+  connectToMongoDB();
+});
