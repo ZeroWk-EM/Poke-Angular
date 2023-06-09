074a447149796148baa898ed5a251b4a5cff60d5
Mirko <mirko180299@gmail.com>
2023-06-09 19:25:55 +0200

    feat(routes): add pokedex route

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import { connectToMongoDB } from "./config/connectionDB.config";
+import authRoutes from "./routes/user.routes";
+import pokedexRoutes from "./routes/pokedex.routes";
 
 const app = express();
 dotenv.config();
@@ -18,9 +20,8 @@ app.get("/", (_: Request, res: Response) => {
 
 const PORT = process.env.PORT || 3000;
 
-import authRoutes from "./routes/user.routes"
-
-app.use("/v1/auth",authRoutes)
+app.use("/v1/auth", authRoutes);
+app.use("/v1/pokedex", pokedexRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server started in port ${PORT} `);
