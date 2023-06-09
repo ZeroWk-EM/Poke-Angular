ce22a9166bccee901cb4062f0edf65796f5866b1
zerowk <em@zerowk.it>
2023-06-09 13:05:27 +0200

    feat(compoent): Define buisness logic for validation-template

--- client/src/components/validate-form/validate-form.component.ts
+++ client/src/components/validate-form/validate-form.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/service/auth.service';
+import { ActivatedRoute } from '@angular/router';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-validate-form',
   templateUrl: './validate-form.component.html',
-  styleUrls: ['./validate-form.component.css']
+  styleUrls: ['./validate-form.component.css'],
 })
-export class ValidateFormComponent {
+export class ValidateFormComponent implements OnInit {
+  idValue: string | null;
+  validateForm!: FormGroup;
 
+  constructor(private authService: AuthService, private route: ActivatedRoute) {
+    this.idValue = this.route.snapshot.paramMap.get('id');
+  }
+
+  ngOnInit() {
+    this.validateForm = new FormGroup({
+      token: new FormControl([this.idValue], Validators.required),
+    });
+  }
+  onSubmit() {
+    this.authService.validate(String(this.idValue)).subscribe({
+      next: (response) => {
+        window.alert(response.message)
+      },
+      error: (error) => {
+        window.alert(error.error.message);
+      },
+    });
+  }
 }
