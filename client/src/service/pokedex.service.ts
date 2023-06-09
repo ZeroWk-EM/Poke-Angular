f7612f17405daf20f92ee6aac9ab10c505a7f428
zerowk <em@zerowk.it>
2023-06-10 00:07:33 +0200

    feat(pokedex): import login for set header

--- client/src/service/pokedex.service.ts
+++ client/src/service/pokedex.service.ts
@@ -1,16 +1,22 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/internal/Observable';
 import { ResponsePokedex } from 'src/models/pokedex';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PokedexService {
-
   constructor(private http: HttpClient) {}
 
   createData = (page: number): Observable<ResponsePokedex> => {
-    return this.http.get<ResponsePokedex>(`http://localhost:5000/v1/pokedex?page=${page}`);
-  }
+    /*  const header = new HttpHeaders({
+      authorization: `${localStorage.getItem('token')}`,
+    }); */
+
+    return this.http.get<ResponsePokedex>(
+      `http://localhost:5000/v1/pokedex?page=${page}` /* ,
+      { headers: header } */
+    );
+  };
 }
