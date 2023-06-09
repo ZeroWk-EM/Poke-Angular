3ef4999463486556cec434222fa3b21b87266a32
zerowk <em@zerowk.it>
2023-06-09 11:55:03 +0200

    feat(compoent): create validate form component

---
+++ client/src/components/validate-form/validate-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-validate-form',
+  templateUrl: './validate-form.component.html',
+  styleUrls: ['./validate-form.component.css']
+})
+export class ValidateFormComponent {
+
+}
