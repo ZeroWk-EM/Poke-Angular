73d9adbed26a080329c0d444575b9d460bb210d4
zerowk <em@zerowk.it>
2023-06-09 13:04:53 +0200

    feat(page): check if isLogged

--- client/src/page/valide-page/valide-page.component.ts
+++ client/src/page/valide-page/valide-page.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-valide-page',
   templateUrl: './valide-page.component.html',
-  styleUrls: ['./valide-page.component.css']
+  styleUrls: ['./valide-page.component.css'],
 })
 export class ValidePageComponent {
+  IsLogged(): boolean {
+    const user = localStorage.getItem('fullname') && localStorage.getItem('token');
 
+    return !!user;
+  }
 }
