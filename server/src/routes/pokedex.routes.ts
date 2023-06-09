ce8316c5cd73e64b045debc1f22f181b08931384
zerowk <em@zerowk.it>
2023-06-10 00:05:44 +0200

    feat(server): add isAuth import into middleware

--- server/src/routes/pokedex.routes.ts
+++ server/src/routes/pokedex.routes.ts
@@ -3,30 +3,40 @@ import express, { response } from "express";
 import axios from "axios";
 import { ResponsePokedex } from "../interface/pokedex.interface";
 import { ResponsePokemon } from "../interface/pokemon.interface";
+import { isAuth } from "../middleware/auth.middleware";
 
-const urlPokedex = "https://pokeapi.co/api/v2/pokemon"
+const urlPokedex = "https://pokeapi.co/api/v2/pokemon";
 
 const router = express.Router();
 router.use(express.json());
 
-router.get(
-  "/", async (req, res) => {
-    try {
-        const response = await axios.get<ResponsePokedex>(`${urlPokedex}?offset=${(Number(req.query.page) - 1)*5 }&limit=5`)
-        const pokemonsDetails = await Promise.all(response.data.results.map(({ url }) => axios.get<ResponsePokemon>(url)))
-        const result = pokemonsDetails.map(({ data: { name, sprites: { front_default }, types, weight } }) => ({
-            name,
-            front_default,
-            types: types.map(({ type: { name }}) => name).join(),
-            weight
-        }));
-        res.status(200).json({ results: result, totalPage: Math.ceil(response.data.count/5) });
-
-    } catch(error) {
-
-    }
-  }
-  
-);
+router.get("/", async (req, res) => {
+  try {
+    const response = await axios.get<ResponsePokedex>(
+      `${urlPokedex}?offset=${(Number(req.query.page) - 1) * 5}&limit=5`
+    );
+    const pokemonsDetails = await Promise.all(
+      response.data.results.map(({ url }) => axios.get<ResponsePokemon>(url))
+    );
+    const result = pokemonsDetails.map(
+      ({
+        data: {
+          name,
+          sprites: { front_default },
+          types,
+          weight,
+        },
+      }) => ({
+        name,
+        front_default,
+        types: types.map(({ type: { name } }) => name).join(),
+        weight,
+      })
+    );
+    res
+      .status(200)
+      .json({ results: result, totalPage: Math.ceil(response.data.count / 5) });
+  } catch (error) {}
+});
 
 export default router;
