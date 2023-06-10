334e867953675f9bcdde77e453b5cf5a5a7b4acd
zerowk <em@zerowk.it>
2023-06-10 18:40:25 +0200

    feat(component): update username

--- client/src/components/navbar/navbar.component.ts
+++ client/src/components/navbar/navbar.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/service/auth.service';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],
 })
-export class NavbarComponent {
-  username = localStorage.getItem('fullname')
-    ? localStorage.getItem('fullname')?.toUpperCase()
-    : 'GUEST';
+export class NavbarComponent implements OnInit {
+  username: string = '';
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.authService.user.subscribe({next: (username) => {
+      this.username = username;
+    }})
+  }
+
+  getStatus(): boolean {
+    return Boolean(localStorage.getItem("token"));
+  }
+
+  exit() {
+    this.authService.logout();
+    this.authService.user.next("GUEST")
+  }
 }
