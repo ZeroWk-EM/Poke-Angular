e0702e6826ca477156d9def90883458ab045ce10
zerowk <em@zerowk.it>
2023-06-10 18:41:52 +0200

    feat(route): add authguard

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -7,14 +7,35 @@ import { LoginPageComponent } from 'src/page/login-page/login-page.component';
 import { PokedexPageComponent } from 'src/page/pokedex-page/pokedex-page.component';
 import { RegisterPageComponent } from 'src/page/register-page/register-page.component';
 import { ValidePageComponent } from 'src/page/valide-page/valide-page.component';
+import { authGuard } from 'src/service/auth.guard';
 const routes: Routes = [
   { path: '', component: HomePageComponent },
-  { path: 'login', component: LoginPageComponent },
-  { path: 'register', component: RegisterPageComponent },
-  { path: 'validate', component: ValidePageComponent },
-  { path: 'validate/:id', component: ValidePageComponent },
-  { path: 'pokedex', component: PokedexPageComponent },
-  { path: 'favorites', component: FavoritePageComponent },
+  { path: 'login', canActivate: [authGuard], component: LoginPageComponent },
+  {
+    path: 'register',
+    canActivate: [authGuard],
+    component: RegisterPageComponent,
+  },
+  {
+    path: 'validate',
+    canActivate: [authGuard],
+    component: ValidePageComponent,
+  },
+  {
+    path: 'validate/:id',
+    canActivate: [authGuard],
+    component: ValidePageComponent,
+  },
+  {
+    path: 'pokedex',
+    canActivate: [authGuard],
+    component: PokedexPageComponent,
+  },
+  {
+    path: 'favorites',
+    canActivate: [authGuard],
+    component: FavoritePageComponent,
+  },
 ];
 
 @NgModule({
