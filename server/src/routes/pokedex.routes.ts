8a5a0de81164f352352d954bc1a84a66b29222c8
zerowk <em@zerowk.it>
2023-06-10 22:34:35 +0200

    refactor(route): move response function into src/controller/pokemon.controller.ts

--- server/src/routes/pokedex.routes.ts
+++ server/src/routes/pokedex.routes.ts
@@ -1,42 +1,10 @@
-import express, { response } from "express";
+import express from "express";
 
-import axios from "axios";
-import { ResponsePokedex } from "../interface/pokedex.interface";
-import { ResponsePokemon } from "../interface/pokemon.interface";
-import { isAuth } from "../middleware/auth.middleware";
-
-const urlPokedex = "https://pokeapi.co/api/v2/pokemon";
+import { getPokemon } from "../controller/pokemon.controller";
 
 const router = express.Router();
 router.use(express.json());
 
-router.get("/", async (req, res) => {
-  try {
-    const response = await axios.get<ResponsePokedex>(
-      `${urlPokedex}?offset=${(Number(req.query.page) - 1) * 5}&limit=5`
-    );
-    const pokemonsDetails = await Promise.all(
-      response.data.results.map(({ url }) => axios.get<ResponsePokemon>(url))
-    );
-    const result = pokemonsDetails.map(
-      ({
-        data: {
-          name,
-          sprites: { front_default },
-          types,
-          weight,
-        },
-      }) => ({
-        name,
-        front_default,
-        types: types.map(({ type: { name } }) => name).join(),
-        weight,
-      })
-    );
-    res
-      .status(200)
-      .json({ results: result, totalPage: Math.ceil(response.data.count / 5) });
-  } catch (error) {}
-});
+router.get("/", getPokemon);
 
 export default router;
