a0531aab35e00f86c25f0f94dac6f0733af25e65
zerowk <em@zerowk.it>
2023-06-11 15:53:18 +0200

    feat(pokemon-card): create Ouptut to send selected pokemon to modal

--- client/src/components/pokemon-card/pokemon-card.component.ts
+++ client/src/components/pokemon-card/pokemon-card.component.ts
@@ -1,13 +1,25 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Pokemon } from 'src/models/pokedex';
 
 @Component({
   selector: 'app-pokemon-card',
   templateUrl: './pokemon-card.component.html',
-  styleUrls: ['./pokemon-card.component.css']
+  styleUrls: ['./pokemon-card.component.css'],
 })
 export class PokemonCardComponent {
-  @Input() name: string = "";
-  @Input() types: string = "";
+  @Input() name: string = '';
+  @Input() types: string = '';
   @Input() weight: number = 0;
-  @Input() img: string = "";
+  @Input() img: string = '';
+
+  @Output() selectPokemon = new EventEmitter<Pokemon>();
+
+  fun = () => {
+    this.selectPokemon.emit({
+      name: this.name,
+      types: this.types,
+      weight: this.weight,
+      front_default: this.img,
+    });
+  };
 }
