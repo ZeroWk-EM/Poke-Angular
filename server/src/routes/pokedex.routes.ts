7aba839e65296f97e13f89d0a48de42bc2dc0852
zerowk <em@zerowk.it>
2023-06-11 11:32:18 +0200

    feat(route): add route for specific pokemon

--- server/src/routes/pokedex.routes.ts
+++ server/src/routes/pokedex.routes.ts
@@ -1,10 +1,10 @@
 import express from "express";
 
-import { getPokemon } from "../controller/pokemon.controller";
+import { getPokemon, getPokemonByName } from "../controller/pokemon.controller";
 
 const router = express.Router();
 router.use(express.json());
 
 router.get("/", getPokemon);
-
+router.get(":name", getPokemonByName);
 export default router;
